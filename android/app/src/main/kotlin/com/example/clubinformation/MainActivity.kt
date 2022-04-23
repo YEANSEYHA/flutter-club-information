5360dab4790c1fa1e00b555d7bd459cdbbe13c8c
YEAN <yeanseyha7@gmail.com>
2022-04-23 15:37:07 +0700

    Init Club football information project

---
+++ android/app/src/main/kotlin/com/example/clubinformation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clubinformation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ClubInfo());
+}
+
+class ClubInfo extends StatelessWidget {
+  const ClubInfo({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'CLub Football Information',
+      theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+        primary: Colors.grey,
+        secondary: Colors.black,
+      )),
+      home: const MyHomePage(title: 'Club Football information'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: SafeArea(
+        child: Text('Football Club Information')
+      ),
+    );
+  }
+}
